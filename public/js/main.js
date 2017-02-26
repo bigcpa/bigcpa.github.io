2880f4e4281b8e3897972fe0ac7a6e2f481903ae
bigcpa <abrakad@openmailbox.org>
2017-02-26 12:53:57 +0300

    auto commit

--- public/js/main.js
+++ public/js/main.js
@@ -62,7 +62,8 @@ $('#fixblock').scroolly([
 	from: 'con-bottom = vp-top',
 	css: {
 		position: 'fixed',
-		top: '0'
+		top: '0',
+    box-shadow: '0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08)'
 	}
 }
 ], $('.sidebar-content'));
